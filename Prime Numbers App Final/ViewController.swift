87ba3ce445988c716d7b17b784b9247c00e1c00c
Spy Name Is Harry <lawrencechen@lawrences-mac.local>
2016-01-29 10:30:34 -0500

    Initial Commit

---
+++ Prime Numbers App Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Numbers App Final
+//
+//  Created by Lawrence Chen on 1/29/16.
+//  Copyright © 2016 Lawrence Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
