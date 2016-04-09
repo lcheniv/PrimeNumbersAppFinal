9acc6942a5eaae9110615506f2abc2b5ffee3574
Lawrence Chen <lawrencechen@Lawrences-Mac.local>
2016-04-08 20:53:26 -0500

    Initial commit

--- Prime Numbers App Final/ViewController.swift
+++ Prime Numbers App Final/ViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var numberTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func isItPrime(sender: AnyObject) {
+        
+        
+        // Removed second ! because now we arn't forcing it - we are testing
+        // if let - avoids crashes
+        if let num = Int(numberTextField.text!) {
+        
+            var isPrime = true
+        
+            if(num == 1){
+                isPrime = false
+            }
+            if(num != 1 && num != 2){
+                for (var i = 2; i < num; i++) {
+                    if (num % i == 0){
+                        isPrime = false
+                    }
+                }
+            }
+        
+            print(isPrime)
+        
+            if(isPrime) {
+            
+                resultLabel.text = "\(num) is prime!"
+            
+            } else {
+            
+                resultLabel.text = "\(num) is not prime."
+            
+            }
+            // End of if statement
+        } else {
+            
+            resultLabel.text = "Please enter a whole number."
+            
+        }
+        
+        }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
